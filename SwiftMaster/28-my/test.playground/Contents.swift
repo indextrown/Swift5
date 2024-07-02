3fe5493ff273a6c1c1c9e82d193588f1147b7955
indextrown <lghan00020@nate.com>
2024-07-03 00:48:36 +0900

    28-my 폴더 생성

---
+++ SwiftMaster/28-my/test.playground/Contents.swift
@@ -0,0 +1,14 @@
+//import UIkit
+
+for i in 2...10 {
+    // let i = 2
+    // let i = 3
+    // let i = 4
+    // ...
+    // let i = 10
+    print(i)
+}
+
+for c in 1...10 {
+    print("안녕하세요")
+}
