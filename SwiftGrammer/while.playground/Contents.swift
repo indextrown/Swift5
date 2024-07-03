b807a222f6fcd34d869e49f978635f85355351be
indextrown <lghan00020@nate.com>
2024-07-03 13:36:35 +0900

    update

--- SwiftGrammer/while.playground/Contents.swift
+++ SwiftGrammer/while.playground/Contents.swift
@@ -1,3 +1,12 @@
-import Cocoa
+// while 조건문
+// while문 내에서는 조건을 변화시킬 수 있는 문장이 들어가야 한다
 
-var greeting = "Hello, playground"
+var sum = 0
+var num = 1
+
+while num <= 10 {
+    print(num)
+    num += 1
+}
+
+
