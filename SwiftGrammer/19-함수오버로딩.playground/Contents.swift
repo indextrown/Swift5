ae563c1d8bdb761b8e8518402d52adcb0679814f
indextrown <lghan00020@nate.com>
2024-07-04 03:41:58 +0900

    test

---
+++ SwiftGrammer/19-함수오버로딩.playground/Contents.swift
@@ -0,0 +1,22 @@
+// 개발자가 쓰기 쉽게 만들어둠
+// 모든 함수를 외울 필요 줄임
+func doSomething(value: Int) {
+    print(value)
+}
+
+func doSomething(value: Double) {
+    print(value)
+}
+
+func doSomething(value: String) {
+    print(value)
+}
+
+func doSomething(_ value: String) {
+    print(value)
+}
+
+func doSomething(value1: Int, value2: Int) {
+    print(value1, value2)
+}
+
