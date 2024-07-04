50f5e896c4bdceebe5427f6ed9ce29e97840a64b
indextrown <lghan00020@nate.com>
2024-07-04 19:33:15 +0900

    upload

--- SwiftGrammer/11-함수오버로딩.playground/Contents.swift
+++ SwiftGrammer/11-함수오버로딩.playground/Contents.swift
@@ -1,5 +1,7 @@
 // 개발자가 쓰기 쉽게 만들어둠
 // 모든 함수를 외울 필요 줄임
+// 함수 이름을 재사용할 수 있다
+// overload: 과적하다, 하나의 이름에 여러 함수를 과적하다
 func doSomething(value: Int) {
     print(value)
 }
@@ -20,3 +22,6 @@ func doSomething(value1: Int, value2: Int) {
     print(value1, value2)
 }
 
+
+
+doSomething(value: "hello")
