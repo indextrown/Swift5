d24bd7d8f5f33fdbd2f96b747d7cfa3571d3c17e
indextrown <lghan00020@nate.com>
2024-07-14 02:47:24 +0900

    bingo upload

--- App/RPSGame/RPSGame/ViewController.swift
+++ App/RPSGame/RPSGame/ViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
+// 클래스 내부에 코드를 작성해야함
 class ViewController: UIViewController {
 
-    // 변수(속성)
+    // 화면과 연결된 변수(속성)
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var comImageView: UIImageView!
     @IBOutlet weak var myImageView: UIImageView!
@@ -17,27 +18,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var comChoiceLabel: UILabel!
     @IBOutlet weak var myChoiceLabel: UILabel!
     
+    /* 데이터 저장을 위한 변수(컴퓨터 선택 / 나의 선택)*/
     // 맨처음에는 무조건 묵 설정
     var myChoice: Rps = Rps.rock
-    
     // 맨 처음에는 컴퓨터의 랜덤값 설정 Rps(rawValue) -> 옵셔널 타입
     var comChoice: Rps = Rps(rawValue: Int.random(in: 0...2))!
     
     // action
-    // 함수(메서드)
-    // 앱의 화면에 들어오면 처음 실행되는 함수
+    // 함수 / 메서드
+    // 앱의 화면에 들어오면 처음 실행되는  함수
     override func viewDidLoad() {
         super.viewDidLoad()
+        /* 속성을 변경시키기 위해 속성에 접근해서 변경하는 부분 */
+        
         // 1) 첫번째 이미지뷰에 준비(묵) 이미지를 띄워야 함
+        // comImageView.image = #imageLiteral(
         comImageView.image = #imageLiteral(resourceName: "ready")
         myImageView.image = UIImage(named: "ready.png")
         
         // 2) 첫번째/두번째 레이블에 "준비"라고 문자열을 띄어야 함
         comChoiceLabel.text = "준비"
         myChoiceLabel.text = "준비"
-        
-        
-        
     }
     
     // 3개의 버튼 중 하나라도 입력해도 실행된다
@@ -45,11 +46,14 @@ class ViewController: UIViewController {
         // 3) 가위 바위 보(enum)를 선택해서 그 정보를 저장해야된다
         // 구별을 할 수 있어야함 -> 어떻게 ??? 파라미터를 활용 sender
         // 가위를 누르면 가위버튼을 눌렀다고 나옴-> 옵셔널 스트링타입 -> 무조건 벗겨야함
-//        guard let title  = sender.currentTitle else {
-//            return
-//        }
+        //        guard let title  = sender.currentTitle else {
+        //            return
+        //        }
+        
         // 버튼의 문자를 가져옴
-        let title = sender.currentTitle!
+        // let title = sender.currentTitle!
+        // 우아한 방식
+        guard let title = sender.currentTitle else { return } // 만족하지 않으면 벗아남
         
         // 버튼의 문자열 비교
         switch title {
---
+++ App/RandomBingo/RandomBingo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  RandomBingo
+//
+//  Created by 김동현 on 7/14/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
