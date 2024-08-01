a2f2659ee53db7ef5e2cb9bd655c13ee90b16261
indextrown <lghan00020@nate.com>
2024-08-01 18:31:42 +0900

    121강 멤버의 확장 업로드

--- SwiftGrammer/70-확장가능 멤버.playground/Contents.swift
+++ SwiftGrammer/70-확장가능 멤버.playground/Contents.swift
@@ -2,7 +2,8 @@
  
  (120강)
  
- 
+ 소급 모델링
+ - 이미 애플이 구현한것 ex) Int, String은 건들 수 없기 때문에 소급해서 적용할 수 있도록 확장에서는 구현 가능하다
  */
 
 // 타입 계산속성의 확장
@@ -58,3 +59,49 @@ extension Int {
 func squared(num: Int) -> Int {
     return num * num
 }
+
+
+// 애플이 만든 타입 메서드 예시
+Int.random(in: 1...100)
+
+// 타입 메서드의 확장
+extension Int {
+    static func printNumberFrom1to5() {
+        for i in 1...5 {
+            print(i)
+        }
+    }
+}
+Int.printNumberFrom1to5()
+
+
+// 인스턴스 메서드의 확장
+// 장점: 문자열 자체를 건들 수 없지만 확장기능을 통해 추가 가능
+// 소급 모델링:
+extension String {
+    func printHelloRepetitions(of times: Int) {
+        for _ in 0..<times {
+            print("Hello \(self)!")
+        }
+    }
+}
+
+"Steve".printHelloRepetitions(of: 4)
+"Swift".printHelloRepetitions(of: 5)
+
+/*
+- mutating 인스턴스 메서드의 확장
+- 구조체나 열거형처럼 값타입일때 사용
+- 메서드에서 원치걱으로 저장속성을 바꿀 수 없는데 구조체, 열거형에서 자신의 속성을 변경할 경우 mutating 키워드 필요
+ 
+ - 구조체나 열거형에서 값을 직접적으로 바꿀때 사용한다
+*/
+
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+
+var someInt = 3
+someInt.square()
