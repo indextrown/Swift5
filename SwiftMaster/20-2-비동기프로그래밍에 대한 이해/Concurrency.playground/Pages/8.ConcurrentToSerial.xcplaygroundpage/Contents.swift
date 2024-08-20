1472b86a3f8b66fde31896c0aade868c2ca46d8c
indextrown <lghan00020@nate.com>
2024-08-20 14:28:33 +0900

    비동기 마무리

--- SwiftMaster/20-2-비동기프로그래밍에 대한 이해/Concurrency.playground/Pages/8.ConcurrentToSerial.xcplaygroundpage/Contents.swift
+++ SwiftMaster/20-2-비동기프로그래밍에 대한 이해/Concurrency.playground/Pages/8.ConcurrentToSerial.xcplaygroundpage/Contents.swift
@@ -7,17 +7,26 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 //: ### Thread-safe하지 않을때, 처리하는 방법
 // 배열은 여러쓰레드에서 동시에 접근하면 문제가 생길 수 있다.
 
-
+// 문자열 배열
+// 빈문자열로 생성
 var array = [String]()
+print(array)
 
 let serialQueue = DispatchQueue(label: "serial")
 
-
+// MARK: 1부터 20까지 배열에 접근해서 출력 및 배열에 append도 가능
 for i in 1...20 {
+    // MARK: 배열은 메모리 공간에 하나만 존재하는데 여러 스레드에서 접근하고 있기 때문에 실제로
     DispatchQueue.global().async {
-        print("\(i)")
+        //print("\(i)")
         //array.append("\(i)")    //  <===== 동시큐에서 실행하면 동시다발적으로 배열의 메모리에 접근
         
+        // MARK: 비동기적으로 append하고 있어서 1부터 20까지의 비동기 처리를 할때 하나의 array메모리 공간에 동시에 접근하게됨
+        // MARK: 전부 안들어갈수도 있음
+        // MARK: 2번스레드에서 append할때 3번스레드에서도 동시에 append 즉 쓰기가 동시에 발생해서 thread safe하지 않은 상황 발생 가능
+        
+        // MARK: 직렬큐로 접근하기 때문에 한번에 하나의 일만 이 배열에 접근할 수 있다
+        // MARK: thread safe하다-> 일반적으로 동시성문제 즉 경쟁상황을 해결하는 방법 중 하나이다
         serialQueue.async {        // 올바른 처리 ⭐️
             array.append("\(i)")
         }
@@ -29,10 +38,12 @@ for i in 1...20 {
 
 // 5초후에 배열 확인하고 싶은 코드 일뿐...
 
+// MARK: asyncAfter = 비동기적으로 메인스레드에서 실행할건데 몇초 후에 실행해
 DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
     print(array)
     //PlaygroundPage.current.finishExecution()
 }
+
 
 
 
