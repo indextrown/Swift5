fef58593b60b2dd69f1eafb99736c8b4a7da7733
김동현 <69367698+indextrown@users.noreply.github.com>
2025-01-13 17:03:54 +0900

    update

---
+++ 4. SwiftSkillUI/SwiftSkillUI/SwiftSkillUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSkillUIApp.swift
+//  SwiftSkillUI
+//
+//  Created by 김동현 on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSkillUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
