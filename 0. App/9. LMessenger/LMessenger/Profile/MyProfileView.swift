85a567e65e0b135111ebbb947890461021d87f65
김동현 <69367698+indextrown@users.noreply.github.com>
2025-01-18 19:32:16 +0900

    update

--- 0. App/9. LMessenger/LMessenger/Profile/MyProfileView.swift
+++ 0. App/9. LMessenger/LMessenger/Profile/MyProfileView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct MyProfileView: View {
+    @Environment(\.dismiss) private var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button {
+                dismiss()
+            } label: {
+                Image("back")
+            }
+        }
     }
 }
 
