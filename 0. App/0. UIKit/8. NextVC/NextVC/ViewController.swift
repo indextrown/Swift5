b118d6a0fcdf0d0f96f8270637da17aabe26944e
indextrown <indextrown@gmail.com>
2025-02-17 22:10:36 +0900

    코드로 스토리보드 객체를 생성해서, 화면 이동 구현 완료

--- 0. App/0. UIKit/8. NextVC/NextVC/ViewController.swift
+++ 0. App/0. UIKit/8. NextVC/NextVC/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     @IBAction func codeNextButtonTapped(_ sender: UIButton) {
         // 인스턴스를 찍어낸다 = 메모리에 올라간다
         let firstVC = FirsstViewController()
-        firstVC.someString = "이 문장을 전달"
+        firstVC.someString = "첫번째 화면"
         firstVC.modalPresentationStyle = .fullScreen
         present(firstVC, animated: true, completion: nil)
     }
@@ -27,22 +27,18 @@ class ViewController: UIViewController {
     // 2) 코드로 스토리보드 객체를 생성해서, 화면 이동
     @IBAction func storyboardWithCodeButtonTapped(_ sender: UIButton) {
 
-        
-        
-        
-        
+        // 스토리보드로 secondViewController 만들었기 때문에 heap영역에 controller, story 각각 따로 존재
+        // as? SecondViewController는 타입캐스팅하여 SecondViewController로 해주는것
+        guard let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as? SecondViewController else { return }
+        secondVC.someString = "두번째 화면"
+        present(secondVC, animated: true, completion: nil)
     }
-    
     
     
     // 3) 스토리보드에서의 화면 이동(간접 세그웨이)
     @IBAction func storyboardWithSegueButtonTapped(_ sender: UIButton) {
         
 
-        
-        
-        
-        
     }
     
 
